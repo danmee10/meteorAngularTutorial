b8dacbd58556c79445404fdb74d68cc39b642d87
Dan Mee <danmee10@gmail.com>
2015-07-15 16:42:20 -0600

    filtering collections

--- simple-todos-angular.js
+++ simple-todos-angular.js
@@ -5,9 +5,21 @@ if (Meteor.isClient) {
 
   app.controller("TodosListCtrl", ['$scope', '$meteor', function($scope, $meteor){
 
-      $scope.tasks = $meteor.collection(function() {
-        return Tasks.find({}, { sort: { createdAt: -1 } })
+      $scope.$watch('hideCompleted', function() {
+        if ($scope.hideCompleted) {
+          $scope.query = {checked: {$ne: true}};
+        } else {
+          $scope.query = {};
+        }
       });
+
+      $scope.tasks = $meteor.collection(function() {
+        return Tasks.find($scope.getReactively('query'), {sort: {createdAt: -1}})
+      });
+
+      $scope.incompleteCount = function () {
+        return Tasks.find({ checked: {$ne: true} }).count();
+      };
 
       $scope.addTask = function(newTask) {
         $scope.tasks.push( {
